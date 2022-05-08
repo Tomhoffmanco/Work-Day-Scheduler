9d3879a3dcb6d7d081cf95f5a60d477e1d69768d
Tom hoffman <tomhoffmanco@gmail.com>
2022-05-08 16:31:47 -0400

    final product

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+var saveBtn = $(".saveBtn");
+
+// current day is displayed at the top of the planner
+$("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
+
+function hour() {
+  var hour = moment().hours();
+
+  $(".time-block").each(function () {
+    var currHour = parseInt($(this).attr("id"));
+
+    // console.log(this); //each time-block
+
+    if (currHour > hour) {
+      $(this).addClass("future");
+    } else if (currHour === hour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
+    }
+  });
+}
+
+// WHEN I click the save button for that time block
+saveBtn.on("click", function () {
+  // console.log(this); //save button
+  var time = $(this).siblings(".hour").text();
+  var plan = $(this).siblings(".plan").val();
+
+  // THEN the text for that event is saved in local storage
+  localStorage.setItem(time, plan);
+});
+
+function scheduler() {
+  $(".hour").each(function () {
+    var currHour = $(this).text();
+    var currPlan = localStorage.getItem(currHour);
+
+    // console.log(this);
+    // console.log(currHour);
+
+    if (currPlan !== null) {
+      $(this).siblings(".plan").val(currPlan);
+    }
+  });
+}
+
+/**
+ * CALL FUNCTIONS
+ */
+
+hour();
+scheduler();
